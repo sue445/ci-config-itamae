41ca06f2cf7d33323422245fb172343c9cec380d
sue445 <sue445@sue445.net>
2020-01-23 21:09:36 +0900

    Impl migration

--- cookbooks/migrate/github_actions/20200123210213_add_continue-on-error_to_codeclimate-test-reporter.rb
+++ cookbooks/migrate/github_actions/20200123210213_add_continue-on-error_to_codeclimate-test-reporter.rb
@@ -2,6 +2,21 @@ file "#{node[:repo]}/.github/workflows/test.yml" do
   action :edit
 
   block do |content|
+    content.gsub!(/^      - name: Setup Code Climate Test Reporter\n(.+?)continue-on-error: (.+?)\n/m) do
+      [
+        "      - name: Setup Code Climate Test Reporter\n",
+        $1,
+        "continue-on-error: true\n"
+      ].join
+    end
+
+    content.gsub!(/^      - name: Teardown Code Climate Test Reporter\n(.+?)continue-on-error: (.+?)\n/m) do
+      [
+        "      - name: Teardown Code Climate Test Reporter\n",
+        $1,
+        "continue-on-error: true\n"
+      ].join
+    end
   end
 
   only_if "ls #{node[:repo]}/.github/workflows/test.yml"
