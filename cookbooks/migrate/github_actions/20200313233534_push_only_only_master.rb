874ee397eff4a0f982c81e76bab4d9b659587cc3
sue445 <sue445@sue445.net>
2020-03-13 23:38:42 +0900

    Run build only master push and PR

--- cookbooks/migrate/github_actions/20200313233534_push_only_only_master.rb
+++ cookbooks/migrate/github_actions/20200313233534_push_only_only_master.rb
@@ -1,8 +1,17 @@
-file "#{node[:repo]}/.github/workflows/test.yml" do
-  action :edit
+%w(test build).each do |name|
+  file "#{node[:repo]}/.github/workflows/#{name}.yml" do
+    action :edit
 
-  block do |content|
+    block do |content|
+      content.gsub!(/on:\n  push:\n(.?)  pull_request:\n/m, <<-YAML)
+on:
+  push:
+    branches:
+      - master
+  pull_request:
+      YAML
+    end
+
+    only_if "ls #{node[:repo]}/.github/workflows/#{name}.yml"
   end
-
-  only_if "ls #{node[:repo]}/.github/workflows/test.yml"
 end
