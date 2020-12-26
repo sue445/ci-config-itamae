665f23a32881f9d68b5a7ca034e46f0230f5d616
sue445 <sue445@sue445.net>
2020-12-26 13:23:01 +0900

    Add ruby:3.0 to build matrix

--- cookbooks/migrate/github_actions/20201226131542_add_ruby_3_0.rb
+++ cookbooks/migrate/github_actions/20201226131542_add_ruby_3_0.rb
@@ -2,6 +2,25 @@ file "#{node[:repo]}/.github/workflows/test.yml" do
   action :edit
 
   block do |content|
+    # using DockerHub ruby
+    unless content.include?("- ruby:3.0")
+      content.gsub!(<<YAML, <<YAML)
+          - ruby:2.7
+YAML
+          - ruby:2.7
+          - ruby:3.0
+YAML
+    end
+
+    # using ruby/setup-ruby
+    unless content.include?('- "3.0"')
+      content.gsub!(<<YAML, <<YAML)
+          - "2.7"
+YAML
+          - "2.7"
+          - "3.0"
+YAML
+    end
   end
 
   only_if "ls #{node[:repo]}/.github/workflows/test.yml"
