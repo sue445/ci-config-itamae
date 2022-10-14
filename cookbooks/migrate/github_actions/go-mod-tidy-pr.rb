237b5febd784882d75b17a0d91d4e9362ac94cb5
sue445 <sue445@sue445.net>
2022-10-14 23:36:31 +0900

    Remove legacy recipes

--- cookbooks/migrate/github_actions/go-mod-tidy-pr.rb
+++
@@ -1,5 +0,0 @@
-template "#{node[:repo]}/.github/workflows/go-mod-tidy-pr.yml" do
-  source "templates/go-mod-tidy-pr.yml.erb"
-
-  only_if "ls #{node[:repo]}/.github/workflows/go-mod-tidy-pr.yml"
-end
--- cookbooks/migrate/upgrade_go_version.rb
+++
@@ -1,3 +0,0 @@
-include_recipe "./definitions/upgrade_go_version"
-
-upgrade_go_version "1.14"
