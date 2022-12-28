29ee028eb0fcad44916aba624e604d1f037b2ccc
sue445 <sue445@sue445.net>
2022-12-29 00:58:21 +0900

    Write recipe

--- cookbooks/migrate/github_actions/20221229005705_add_ruby_3_2.rb
+++ cookbooks/migrate/github_actions/20221229005705_add_ruby_3_2.rb
@@ -2,6 +2,25 @@ file "#{node[:repo]}/.github/workflows/test.yml" do
   action :edit
 
   block do |content|
+    # using DockerHub ruby
+    unless content.include?("- ruby:3.2")
+      content.gsub!(<<YAML, <<YAML)
+          - ruby:3.1
+YAML
+          - ruby:3.1
+          - ruby:3.2
+YAML
+    end
+
+    # using ruby/setup-ruby
+    unless content.include?('- "3.2"')
+      content.gsub!(<<YAML, <<YAML)
+          - "3.1"
+YAML
+          - "3.1"
+          - "3.2"
+YAML
+    end
   end
 
   only_if "ls #{node[:repo]}/.github/workflows/test.yml"
