84c0d6d2799e1c0eff3f9fb31a1f912fbf7b999b
sue445 <sue445@sue445.net>
2024-04-11 01:06:19 +0900

    Migrate to act10ns/slack

--- cookbooks/migrate/github_actions/20240410235103_migrate_to_act10ns_slack.rb
+++ cookbooks/migrate/github_actions/20240410235103_migrate_to_act10ns_slack.rb
@@ -5,6 +5,20 @@ workflow_files.each do |workflow_file|
     action :edit
 
     block do |content|
+      if content.include?("lazy-actions/slatify")
+        content.gsub!(%r{lazy-actions/slatify@.+}, "act10ns/slack@v2")
+
+        content.gsub!(%r{          job_name:.*matrix.*\n          type:.+\n          icon_emoji:.+\n          url:.+\n          token:.+\n}, <<YML)
+          status: ${{ job.status }}
+          webhook-url: ${{ secrets.SLACK_WEBHOOK }}
+          matrix: ${{ toJson(matrix) }}
+YML
+
+        content.gsub!(%r{          job_name:.+\n          type:.+\n          icon_emoji:.+\n          url:.+\n          token:.+\n}, <<YML)
+          status: ${{ job.status }}
+          webhook-url: ${{ secrets.SLACK_WEBHOOK }}
+YML
+      end
     end
 
     only_if "ls #{workflow_file}"
