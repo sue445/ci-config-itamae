50e4bfe260168633bfba8b791163aba4141c3c64
sue445 <sue445@sue445.net>
2024-04-11 22:21:32 +0900

    CI against for go 1.22

--- cookbooks/migrate/github_actions/20240411222022_add_go_1_22.rb
+++ cookbooks/migrate/github_actions/20240411222022_add_go_1_22.rb
@@ -5,6 +5,16 @@ workflow_files.each do |workflow_file|
     action :edit
 
     block do |content|
+      unless content.include?('- "1.22"')
+        content.gsub!(<<YAML, <<YAML)
+          - "1.21"
+YAML
+          - "1.21"
+          - "1.22"
+YAML
+      end
+
+      content.gsub!(/go-version: "[0-9.]+"/, 'go-version: "1.22"')
     end
 
     only_if "ls #{workflow_file}"
