1670cff7fd0e1f5b48c023cf43797220368ba1cf
sue445 <sue445@sue445.net>
2025-12-17 22:08:50 +0900

    notify -> all-pass

--- cookbooks/migrate/github_actions/20251217220049_all_pass.rb
+++ cookbooks/migrate/github_actions/20251217220049_all_pass.rb
@@ -5,6 +5,25 @@ workflow_files.each do |workflow_file|
     action :edit
 
     block do |content|
+      content.gsub!(<<-YAML, <<-YAML)
+  notify:
+    needs:
+      YAML
+  all-pass:
+    if: always()
+
+    needs:
+      YAML
+
+      content.gsub!(/    runs-on: ubuntu-latest\n+    steps:\n      - name: Slack Notification \(success\).+webhook-url: \${{ secrets\.SLACK_WEBHOOK }}\n/m, <<-YAML)
+    runs-on: ubuntu-slim
+
+    steps:
+      - name: check dependent jobs
+        uses: re-actors/alls-green@05ac9388f0aebcb5727afa17fcccfecd6f8ec5fe # v1.2.2
+        with:
+          jobs: ${{ toJSON(needs) }}
+      YAML
     end
 
     only_if "ls #{workflow_file}"
