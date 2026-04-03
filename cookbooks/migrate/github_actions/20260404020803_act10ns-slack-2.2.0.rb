47a4b75fd435586e49a0ea75f92d9b6f59ee42b7
sue445 <sue445@sue445.net>
2026-04-04 02:08:10 +0900

    ./bin/generate --template=github_actions act10ns-slack-2.2.0

---
+++ cookbooks/migrate/github_actions/20260404020803_act10ns-slack-2.2.0.rb
@@ -0,0 +1,12 @@
+workflow_files = Dir.glob("#{node[:repo]}/.github/workflows/*.yml")
+
+workflow_files.each do |workflow_file|
+  file workflow_file do
+    action :edit
+
+    block do |content|
+    end
+
+    only_if "ls #{workflow_file}"
+  end
+end
