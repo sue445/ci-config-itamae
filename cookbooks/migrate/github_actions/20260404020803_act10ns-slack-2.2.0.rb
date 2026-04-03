6e78e94748f6f35299f9cc4c39001080820a3bfe
sue445 <sue445@sue445.net>
2026-04-04 02:09:41 +0900

    Impl act10ns-slack-2.2.0

--- cookbooks/migrate/github_actions/20260404020803_act10ns-slack-2.2.0.rb
+++ cookbooks/migrate/github_actions/20260404020803_act10ns-slack-2.2.0.rb
@@ -5,6 +5,7 @@ workflow_files.each do |workflow_file|
     action :edit
 
     block do |content|
+      content.gsub!(%r{uses: act10ns/slack@.*$}, "uses: act10ns/slack@d96404edccc6d6467fc7f8134a420c851b1e9054 # v2.2.0")
     end
 
     only_if "ls #{workflow_file}"
